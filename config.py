4343e999b5ab42b733d52f8954aeea2562fc1156
DREX <47825463+Drex-Dbm@users.noreply.github.com>
2020-04-02 17:37:23 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Basic Config (For all bots)
-userToken = [''] # User Token here, multiple accounts may work. example 'token','token'
-DiscordChannel = '' # Channel ID,
+userToken = ['NTM2MjQ3NzM3NzA5MTAxMDU2.DyT7mg.O3HPxz1We6VA5-2xtx52SLqDpAM','NTM2MjQ4MTQ0NTk0MzM3ODI5.DyT7sg.PxUDNRk2oKuwj4zSxERQbUuUujs','NTM2MjQ5MTg1Mjc2Nzg4NzM2.DyT8iQ.FmPmqbdXaLZma2GnNstitP2dsC8'] # User Token here, multiple accounts may work. example 'token','token'
+DiscordChannel = '596762549547040790' # Channel ID,
 
 #Picture spam
 DirPictures = 'C:\\Users\\Public\\Pictures\\Cheese\\Pepper Jack\\' # Picture(s) location !ALWAYS DOUBLE SLASH AND END WITH THAT DOUBLE SLASH YO!
